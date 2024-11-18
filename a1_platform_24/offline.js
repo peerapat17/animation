1320b0211b66a15e98a718fb4e00b598c7f48aa8
peerapat17 <s42882@bj.ac.th>
2024-11-18 15:07:58 +0700

    Add files via upload

---
+++ a1_platform_24/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1731915718,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/player-sheet0.png",
+		"images/coin-sheet0.png",
+		"images/coinbox-sheet0.png",
+		"images/background.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
